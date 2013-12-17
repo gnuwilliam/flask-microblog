0ceb5efb44cb1048273885a55b19f1ddcaa09a7f
William Oliveira <sudowilliam@gmail.com>
2013-12-17 15:44:36 -0200

    adds base template

--- app/views.py
+++ app/views.py
@@ -5,6 +5,13 @@ from app import app
 @app.route('/index')
 def index():
     user = { 'nickname': 'William' }
+    posts = [
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Yay, posting on Flask'
+        }
+    ]
+    
     return render_template('index.html',
                            title = 'Home',
                            user = user)
